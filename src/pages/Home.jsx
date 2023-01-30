af202fad4548fe6e8ee5403ce402368a03ce821a
M0mahmoud <houdmohamed85@gmail.com>
2023-01-30 03:30:58 +0200

    Phone Version

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -4,10 +4,10 @@ import { Link } from "react-router-dom";
 const Home = () => {
   return (
     <main className="welcome mt-5 text-center">
-      <h3>Welcome to React Chat.</h3>
-      <p>Sign in with Google to chat with with your fellow React Developers.</p>
-      <div className="mt-5">
-        <Link className="btn btn-primary px-3 me-3" to="/signup">
+      <h3>React Chat</h3>
+      <p>A great place to share your thoughts with friends</p>
+      <div className="mt-5 d-flex justify-content-center flex-column ">
+        <Link className="btn btn-primary px-3  mb-3" to="/signup">
           Create New Account
         </Link>
         <Link className="btn btn-outline-primary px-3" to="/login">
